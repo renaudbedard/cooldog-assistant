a6cbed64b19a26ec638471308b958b2096912c04
Renaud Bédard <renaud.bedard@gmail.com>
2016-01-30 18:42:56 -0500

    Animation mappings

---
+++ Assets/Scripts/Cooldog.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Cooldog : MonoBehaviour
+{
+	public struct SpriteMapping
+	{
+		public string Body;
+		public string Face;
+		public string Headdress;
+		public string Arms;
+		public string Eyes;
+		public string NeckDecoration;
+	}
+
+	public Dictionary<string, string> DefaultSet;
+
+	void Start() 
+	{
+	
+	}
+	
+	void Update()
+	{
+	
+	}
+}
---
+++ Assets/Scripts/DogPart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class DogPart : MonoBehaviour 
+{
+	[Serializable]
+	public struct SpriteOption
+	{
+		public string Name;
+		public Sprite Sprite;
+	}
+
+	public SpriteOption[] Sprites;
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+	
+	}
+}
