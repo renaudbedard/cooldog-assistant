4de98655a82cac725bba1e107e2d3b3beecc7d19
Zander <zander@uken.com>
2016-01-31 13:13:09 -0500

    More commands

--- Assets/Scripts/CommandParser.cs
+++ Assets/Scripts/CommandParser.cs
@@ -15,7 +15,11 @@ public class CommandParser : MonoBehaviour {
 			{ "hello", SayHello },
 			{ "hi", SayHello },
 			{ "sup", SayHello },
-			{ "batman", BecomeBatman }
+			{ "batman", BecomeBatman },
+			{ "email", OpenEmail },
+			{ "note", TakeNotes },
+			{ "memo", TakeNotes },
+			{ "remember", TakeNotes }
 		};
 
 		inputField.onEndEdit.AddListener(val => {
@@ -47,13 +51,39 @@ public class CommandParser : MonoBehaviour {
 
 	private void BecomeBatman(){
 		List<DialoguePart> parts = new List<DialoguePart> ();
-		parts.Add (new DialoguePart ("yes im batdog", 2f));
+		parts.Add (new DialoguePart ("yes im batdog", 1f));
 
 		Cooldog cooldog = GameObject.Find ("Cooldog").GetComponent<Cooldog>();
 		StartCoroutine (cooldog.ChangeCostume ("Batman"));
 
 		TextTyper typer = GameObject.Find("Cooldog").GetComponent<TextTyper>();
 		typer.Play (parts);
+	}
+
+	private void TakeNotes() {
+		List<DialoguePart> parts = new List<DialoguePart> ();
+		parts.Add (new DialoguePart ("lets just write that down. yeah.", 2f));
+
+		Cooldog cooldog = GameObject.Find ("Cooldog").GetComponent<Cooldog> ();
+		StartCoroutine (cooldog.ChangeCostume ("Picture"));
+
+		TextTyper typer = GameObject.Find ("Cooldog").GetComponent<TextTyper> ();
+		typer.Play (parts);
+
+		System.Diagnostics.Process.Start("notepad.exe");
+	}
+
+	private void OpenEmail() {
+		List<DialoguePart> parts = new List<DialoguePart> ();
+		parts.Add (new DialoguePart ("an email? i can help with that", 2f));
+
+		Cooldog cooldog = GameObject.Find ("Cooldog").GetComponent<Cooldog> ();
+		StartCoroutine (cooldog.ChangeCostume ("Normal"));
+
+		TextTyper typer = GameObject.Find ("Cooldog").GetComponent<TextTyper> ();
+		typer.Play (parts);
+
+		Application.OpenURL ("http://www.dogpile.com/search/web?q=cool+email+for+dogs");
 	}
 
 }
