8d1a7f28560cb69c27a241a67d1b8c5826b1c67b
Renaud Bédard <renaud.bedard@gmail.com>
2016-01-31 15:19:00 -0500

    Poop detection

--- Assets/Scripts/PoopProduction.cs
+++ Assets/Scripts/PoopProduction.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using System.Collections;
 
@@ -10,9 +11,11 @@ public class PoopProduction : MonoBehaviour
 	int poops;
 	string exeDirectory;
 	byte[] crunchedPoop;
+	bool waitingForCleanup;
 
 	Cooldog cooldog;
 	AudioSource audioSource;
+	TextTyper textTyper;
 
 	public AudioClip[] FartSounds;
 
@@ -28,16 +31,47 @@ public class PoopProduction : MonoBehaviour
 
 		audioSource = GetComponent<AudioSource>();
 		cooldog = GetComponent<Cooldog>();
+		textTyper = GetComponent<TextTyper>();
+	}
+
+	private void CheckPoopCount()
+	{
+		var directory = new DirectoryInfo(exeDirectory);
+		var poopCount = directory.GetFiles("Poop*.png").Length;
+		if (poopCount > 2)
+		{
+			textTyper.Play(new List<DialoguePart>
+			{
+				new DialoguePart("gettin' smelly in here", 1.5f),
+				new DialoguePart(string.Format("there's like {0} poops around", poopCount), 1.5f),
+				new DialoguePart("would appreciate some housekeeping", 1.5f),
+			});
+			waitingForCleanup = true;
+		}
+	}
+
+	void CheckForClean()
+	{
+		var directory = new DirectoryInfo(exeDirectory);
+		var poopCount = directory.GetFiles("Poop*.png").Length;
+		if (poopCount == 0)
+		{
+			textTyper.Play(new List<DialoguePart>
+			{
+				new DialoguePart("aw thanks man, all clean and nice here now", 2f),
+			});
+			waitingForCleanup = false;
+		}
 	}
 	
 	void Update()
 	{
 		poopTimer -= Time.deltaTime;
 		if (poopTimer <= 0 && !Poopin && !cooldog.Blinking)
-		{
 			StartCoroutine(Poop());
-			poopTimer = PoopDelay * UnityEngine.Random.Range(0.8f, 1.2f);
-		}
+
+		if (waitingForCleanup)
+			CheckForClean();
 	}
 
 	IEnumerator Poop()
@@ -64,6 +98,9 @@ public class PoopProduction : MonoBehaviour
 #endif
 
 		yield return StartCoroutine(cooldog.PoopPhase2());
+
+		poopTimer = PoopDelay * UnityEngine.Random.Range(0.8f, 1.2f);
+		CheckPoopCount();
 
 		Poopin = false;
 	}
